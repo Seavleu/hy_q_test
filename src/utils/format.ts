1964a5870b3b9330b5f234e5f330967d642fabad
Jenny Heang <86590058+Seavleu@users.noreply.github.com>
2024-12-05 16:42:27 +0900

    remove: deleted uncessary format utils

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -5,15 +5,3 @@ export const returnToLocaleString = (val: string | number | null | undefined): s
     }
     return '-'
 }
-
-export const formatTime = (time: string | null | undefined): string => {
-    return time ? `${time.slice(0, 2)}:${time.slice(2)}` : '00:00'
-}
-
-export const formatTotal = (val: string | number | null | undefined): string => {
-    if (val !== null && val !== undefined) {
-        const num = Number(val)
-        return num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
-    }
-    return '-'
-}
